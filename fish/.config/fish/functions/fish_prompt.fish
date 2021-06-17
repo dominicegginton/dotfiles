6769514f47c1e2a61564194a3c238d9581550723
Dominic Egginton <dominic.egginton@gmail.com>
2021-06-17 22:16:01 +0100

    lint

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -36,10 +36,7 @@ end
 
 function fish_prompt
   set -l last_status $status
-
   _print_in_color "\n"(prompt_pwd) blue
-
   __fish_git_prompt " %s"
-
   _print_in_color "\n❯ " (_prompt_color_for_status $last_status)
 end
