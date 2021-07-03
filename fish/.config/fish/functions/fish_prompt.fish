f756863604b7fc277295b919414fa834f605f929
Dominic Egginton <dominic.egginton@gmail.com>
2021-07-03 20:00:31 +0100

    set git prompt config fin fish

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,25 +1,35 @@
-set fish_prompt_pwd_dir_length 0
-
-# Git prompt
+# GIT PROMPT
 set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch magenta
-set __fish_git_prompt_color_dirtystate yellow
-set __fish_git_prompt_color_stagedstate blue
-set __fish_git_prompt_color_upstream blue
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'verbose'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_describe_style 'default'
+set __fish_git_prompt_showcolorhints 'yes'
+set __fish_git_prompt_char_stateseparator ' '
 
-# Git Characters
-set __fish_git_prompt_char_dirtystate '*'
-set __fish_git_prompt_char_stagedstate '⇢'
+# GIT CHARACTERS
+set __fish_git_prompt_char_cleanstate ''
+set __fish_git_prompt_char_dirtystate 'M '
+set __fish_git_prompt_char_invalidstate '# '
+set __fish_git_prompt_char_stagedstate '⇢ '
+set __fish_git_prompt_char_untrackedfiles 'U'
 set __fish_git_prompt_char_upstream_prefix ' '
 set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_ahead '⇡'
 set __fish_git_prompt_char_upstream_behind '⇣'
 set __fish_git_prompt_char_upstream_diverged '⇡⇣'
 
-function _print_in_color
-  set -l string $argv[1]
-  set -l color  $argv[2]
+# GIT COLORS
+set __fish_git_prompt_color_branch magenta
+set __fish_git_prompt_color_branch_detached red
+set __fish_git_prompt_color_flags yellow
+set __fish_git_prompt_color_cleanstate green
+set __fish_git_prompt_color_dirtystate yellow
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_stashstate blue
+set __fish_git_prompt_color_untrackedfiles green
+set __fish_git_prompt_color_upstream blue
 
   set_color $color
   printf $string
