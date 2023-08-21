a72ee938e34b00604a73b3ee21137f7c82e469a8
Dominic Egginton <dominic.egginton@gmail.com>
2023-08-21 10:01:04 +0100

    chore(workstation-core): update nvim cmp config

--- workstation-core/.config/nvim/lua/dom/modules/lsp/cmp.lua
+++ workstation-core/.config/nvim/lua/dom/modules/lsp/cmp.lua
@@ -5,7 +5,14 @@ local vscode_loaders = require('luasnip.loaders.from_vscode')
 
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vscode_loaders.lazy_load()
+
 local cmp_expand = function(args) luasnip.lsp_expand(args.body) end
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
   snippit = { expand = cmp_expand },
   window = { documentation = cmp.config.window.bordered() },
@@ -16,6 +23,28 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<Down>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<Up>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
