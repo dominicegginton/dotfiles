d846e7ae45bfd34685ca778a3ff94344099eae80
Dominic Egginton <dominic.egginton@gmail.com>
2023-09-14 11:17:32 +0100

    feat(workspace): format nix flake

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { nixpkgs, home-manager,  ... }:
-  { 
+  {
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-   
+
     nixosConfigurations = {
       latitude-7390 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -31,8 +31,10 @@
     };
 
     homeConfigurations = {
-      "dominic.egginton@macos" = home-manager.lib.homeManagerConfiguration {
-        modules = [];
+      "dominic.egginton@MCCML44WMD6T" = home-manager.lib.homeManagerConfiguration {
+        modules = [
+          ./home-manager/home.nix
+        ];
       };
     };
   };
