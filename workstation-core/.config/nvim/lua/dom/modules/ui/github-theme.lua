dc0dea90097be7846e16017ad230370d142e1eb2
Dominic Egginton <dominic.egginton@gmail.com>
2023-09-14 11:17:04 +0100

    feat(workstation-core): add border

--- workstation-core/.config/nvim/lua/dom/modules/ui/github-theme.lua
+++ workstation-core/.config/nvim/lua/dom/modules/ui/github-theme.lua
@@ -6,21 +6,11 @@ github_theme.setup({
     hide_end_of_buffer = true,
     hide_nc_statusline = true,
     transparent = false,
-    terminal_colors = true,
     dim_inactive = true,
-    module_default = true,
-    inverse = {
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
     darken = {
       floats = true,
       sidebars = {
         enabled = true,
-        list = {
-          'Telescope',
-        },
       },
     },
   },
--- workstation-core/.config/nvim/lua/dom/modules/ui/which-key.lua
+++ workstation-core/.config/nvim/lua/dom/modules/ui/which-key.lua
@@ -2,7 +2,7 @@ local which_key = require('which-key')
 
 which_key.setup({
   window = {
-    border = 'none',
+    border = 'single',
   },
 })
 which_key.register({ t = 'Toggle', f = 'Find', d = 'Debugging' }, { prefix = '<leader>' })
