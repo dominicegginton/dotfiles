c566c3cc8c01c7ab32f63256eecaa3ddf09e9624
Dominic Egginton <dominic.egginton@gmail.com>
2023-09-27 10:26:56 +0100

    chore(workspace): tidy unused dotfiles now managed by nix

--- workstation-linux/.config/sway/clamshell-mode.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/sh
-
-if grep -q open /proc/acpi/button/lid/LID/state; then
-  swaymsg output eDP-1 enable
-else
-  swaymsg output eDP-1 disable
-fi
-
--- workstation-macos/.local/utils/macos.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/env sh
-
-update_macos () {
-  sudo softwareupdate --install --all
-}
