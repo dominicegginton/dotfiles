53909ff2336ebcdf8d88e3f41bbaf1ead8b2a4fd
Dominic Egginton <dominic.egginton@gmail.com>
2023-10-05 18:43:42 +0100

    feat(hosts/latitude-7390): better power management and cpu scheduling

--- hosts/latitude-7390/configuration.nix
+++ hosts/latitude-7390/configuration.nix
@@ -47,6 +47,20 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  powerManagement.powertop.enable = true;
+
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+  services.power-profiles-daemon.enable = false;
+  services.thermald.enable = true;
   services.upower.enable = true;
   services.printing.enable = true;
   services.blueman.enable = true;
