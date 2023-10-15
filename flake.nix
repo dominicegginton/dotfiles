bf91c4303243f721b98002d4b7d7a0b14c20c327
Dominic Egginton <dominic.egginton@gmail.com>
2023-10-15 22:45:59 +0100

    chore(workspace): add nixos hardware and disko inputs

--- flake.nix
+++ flake.nix
@@ -5,8 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
