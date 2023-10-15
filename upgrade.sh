66b83cca31ec5b1f8a5883b818a50ede0e8990fa
Dominic Egginton <dominic.egginton@gmail.com>
2023-10-15 14:56:21 +0100

    chore(workspace): remove upgrade script

--- upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-script_path=$(dirname "$(realpath "$0")")
-
-sudo ln -sf "$script_path"/flake.nix /etc/nixos/flake.nix
-sudo nix-channel --update
-sudo nixos-rebuild switch --flake ".#$1" --upgrade-all
-sudo nix-env -u '*'
