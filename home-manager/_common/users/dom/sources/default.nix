8134b78d1b2537d9fcae777fdcbe983a6de03ebb
Dominic Egginton <dominic.egginton@gmail.com>
2023-10-19 11:22:29 +0100

    fix(home-manger): update alacritty config and tidy sources

--- home-manager/_common/users/dom/sources/default.nix
+++ home-manager/_common/users/dom/sources/default.nix
@@ -1,36 +1,23 @@
+{ pkgs, ... }:
+
+let
+  inherit (pkgs.stdenv) isDarwin;
+in
+
 {
   home.file = {
-    ".config" = {
-      source = ../sources/.config;
-      recursive = true;
-    };
-
-    ".arup.gitconfig" = {
-      source = ../sources/.arup.gitconfig;
-    };
-
-    ".editorconfig" = {
-      source = ../sources/.editorconfig;
-    };
-
-    ".gitconfig" = {
-      source = ../sources/.gitconfig;
-    };
-
-    ".gitignore" = {
-      source = ../sources/.gitignore;
-    };
-
-    ".gitmessage" = {
-      source = ../sources/.gitmessage;
-    };
-
-    ".npmrc" = {
-      source = ../sources/.npmrc;
-    };
-
-    "background.jpg" = {
-      source = ../sources/background.jpg;
-    };
+    ".config".source = ../sources/.config;
+    ".config".recursive = true;
+    ".config/alacritty/fonts.yml".source =
+      if isDarwin
+      then ../sources/.config/alacritty/fonts.darwin.yml
+      else ../sources/.config/alacritty/fonts.linux.yml;
+    ".arup.gitconfig".source = ../sources/.arup.gitconfig;
+    ".editorconfig".source = ../sources/.editorconfig;
+    ".gitconfig".source = ../sources/.gitconfig;
+    ".gitignore".source = ../sources/.gitignore;
+    ".gitmessage".source = ../sources/.gitmessage;
+    ".npmrc".source = ../sources/.npmrc;
+    "background.jpg".source = ../sources/background.jpg;
   };
 }
