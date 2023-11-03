195894ebe61c5f1361b2fc4ce55d561ca4a28005
Dominic Egginton <dominic.egginton@gmail.com>
2023-11-03 15:40:46 +0000

    chore(home-manager): temp remove quartz desktop config for dom

--- home-manager/_common/users/dom.egginton/desktop/quartz.nix
+++ home-manager/_common/users/dom.egginton/desktop/quartz.nix
@@ -1,24 +1,24 @@
 { config, lib, ... }:
 
 {
-  homebrew.brewPrefix = "/opt/homebrew/bin";
-
-  homebrew.onActivation.autoUpdate = true;
-  homebrew.onActivation.cleanup = "zap";
-  homebrew.enable = true;
-
-  homebrew.global.brewfile = true;
-  homebrew.global.lockfiles = false;
-
-  homebrew.taps = [ ];
-
-  homebrew.brews = [ ];
-
-  homebrew.casks = [
-    "figma"
-    "firefox-developer-edition"
-    "google-chrome-dev"
-    "grammarly-desktop"
-    "slack"
-  ];
+  # homebrew.brewPrefix = "/opt/homebrew/bin";
+  #
+  # homebrew.onActivation.autoUpdate = true;
+  # homebrew.onActivation.cleanup = "zap";
+  # homebrew.enable = true;
+  #
+  # homebrew.global.brewfile = true;
+  # homebrew.global.lockfiles = false;
+  #
+  # homebrew.taps = [ ];
+  #
+  # homebrew.brews = [ ];
+  #
+  # homebrew.casks = [
+  #   "figma"
+  #   "firefox-developer-edition"
+  #   "google-chrome-dev"
+  #   "grammarly-desktop"
+  #   "slack"
+  # ];
 }
