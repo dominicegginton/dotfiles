b523a7628e89532592108104d6c961bd68089a52
Dominic Egginton <dominic.egginton@gmail.com>
2023-11-11 16:01:39 +0000

    chore(nixos): update common console zsh config

--- nixos/_common/console/zsh.nix
+++ nixos/_common/console/zsh.nix
@@ -2,7 +2,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     promptInit = "autoload -U promptinit && promptinit";
   };
 }
