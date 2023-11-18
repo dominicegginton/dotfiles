0147a58c743e5753ac3c5d3f37c712f8aeb30fe4
Dominic Egginton <dominic.egginton@gmail.com>
2023-11-18 15:53:04 +0000

    refactor(pkgs): move to flat file structure

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,15 @@
 {pkgs ? (import ../nixpkgs.nix) {}}: let
   inherit (pkgs) callPackage;
 in {
+  # system utils
+  rebuild-host = callPackage ./rebuild-host.nix {};
+  rebuild-iso-console = callPackage ./rebuild-iso-console.nix {};
+  # user utils
+  rebuild-home = callPackage ./rebuild-home.nix {};
+  gpg-import-keys = callPackage ./gpg-import-keys.nix {};
+  # network utils
+  network-filters-disable = callPackage ./network-filters-disable.nix {};
+  network-filters-enable = callPackage ./network-filters-enable.nix {};
   # node packages
   my.nodePackages.custom-elements-languageserver = callPackage ./node-packages/custom-elements-languageserver.nix {};
-  gpg-import-keys = callPackage ./gpg/import-keys.nix {};
-  network-filters-enable = callPackage ./network-filters/enable.nix {};
-  network-filters-disable = callPackage ./network-filters/disable.nix {};
-  rebuild-home = callPackage ./rebuild/home.nix {};
-  rebuild-host = callPackage ./rebuild/host.nix {};
-  rebuild-iso-console = callPackage ./rebuild/iso-console.nix {};
 }
