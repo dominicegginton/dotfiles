e2c6fa8b162b0a6f1070a1d27208579bd17502b4
Dominic Egginton <dominic.egginton@gmail.com>
2023-12-02 15:33:01 +0000

    fix(home-manager): update waybay styling to fix padding and spacing and add bemenu params

--- home-manager/desktop/sway.nix
+++ home-manager/desktop/sway.nix
@@ -24,7 +24,7 @@
         style = "Bold Semi-Condensed";
         size = 11.0;
       };
-      menu = "bemenu-run -n";
+      menu = "bemenu-run -nb -l 10 -H 22 -W 0.4 --fn 'Ubuntu' --tb '#000000' --tf '#ffffff' --fb '#000000' --ff '#ffffff' --nb '#000000' --nf '#ffffff' --hb '#000000' --hf '#ffffff'";
       bars = [{command = "waybar";}];
       colors = {
         focused = {
@@ -100,15 +100,16 @@
           "sway/workspaces"
           "sway/mode"
           "sway/scratchpad"
+          "sway/window"
         ];
         modules-center = [];
         modules-right = [
           "pulseaudio"
           "network"
           "backlight"
-          "battery"
           "clock"
           "tray"
+          "battery"
         ];
         "sway/workspaces" = {
           disable-scroll = true;
@@ -122,6 +123,26 @@
             "5" = "";
             urgent = "";
             focused = "";
+            default = "";
+          };
+        };
+        "sway/mode" = {
+          "format" = "{mode}";
+          "format-icons" = {
+            default = "";
+            resize = "";
+            move = "";
+          };
+        };
+        "sway/scratchpad" = {
+          "format" = "{count}";
+          "format-icons" = {
+            default = "";
+          };
+        };
+        "sway/window" = {
+          "format" = "{title}";
+          "format-icons" = {
             default = "";
           };
         };
@@ -177,73 +198,185 @@
       };
     };
     style = ''
-       * {
-         border: none;
-         border-radius: 0;
-         font-size: 14px;
-         min-height: 0;
-         color: #FFFFFF;
-         background: #000000;
-       }
+      * {
+        border: none;
+        border-radius: 0px;
+        font-family: Ubuntu Nerd Font;
+        font-size: 16px;
+        font-weight: bold;
+        min-height: 25px;
+      }
+      window#waybar {
+        background-color: rgba(242, 242, 242, 1.0);
+        border-bottom: 0px solid rgba(100, 114, 125, 0.5);
+        color: #595959;
+        transition-property: background-color;
+        transition-duration: .5s;
+        border-radius: 0px;
+      }
 
-       window#waybar {
-         background: #000000;
-       }
+      window#waybar.hidden {
+        opacity: 0;
+      }
 
-       #workspaces button {
-         padding: 0 15px 0 0;
-       }
+      window#waybar.empty #window {
+        background: none;
+      }
 
-       #workspaces button.focused {
-         color: #FFFFFF;
-         background: #0366D6;
-       }
-
-       #workspaces button.urgent {
-         color: #000000;
-         background-color: #DBAB09;
-       }
-
-       #mode {
-         background: #000000;
-         border-bottom: none;
-       }
-
-       #clock,
-       #battery,
-       #backlight,
-       #network,
-       #pulseaudio,
-       #tray,
-       #mode,
-       #idle_inhibitor {
-         padding: 0 10px;
-         margin: 0 0px;
-       }
-
+      #workspaces button {
+        padding: 2px 10px;
+        margin-right: 5px;
+        background-color: transparent;
+        color: #595959;
+        box-shadow: inset 0px -0px transparent;
+        border-radius: 0px;
+      }
+      #workspaces button:hover {
+        padding: 2px 10px;
+        margin-right: 5px;
+        background: rgba(196, 196, 196, 0.7);
+        color: #000000;
+        box-shadow: inset 0px -0px #ffffff;
+        outline-style: none;
+        text-shadow: none;
+      }
+      #workspaces button.focused {
+        padding: 2px 10px;
+        margin-right: 5px;
+        background-color: #c4c4c4;
+        color: #000000;
+        box-shadow: inset 0px -0px #000000;
+        outline-style: none;
+        text-shadow: none;
+      }
+      #workspaces button.urgent {
+        padding: 2px 10px;
+        margin-right: 5px;
+        background-color: #ff8f88;
+        color: #ffffff;
+        outline-style: none;
+        text-shadow: none;
+      }
+      #battery,
+      #cpu,
+      #memory,
+      #temperature,
+      #backlight,
+      #network,
+      #pulseaudio,
+      #custom-media,
+      #tray,
+      #mode,
+      #custom-date,
+      #custom-usbdrive,
+      #custom-power,
+      #custom-emacsd,
+      #scratchpad,
+      #window,
+      #mpd,
+      #idle_inhibitor {
+        padding: 2px 10px;
+        margin: 0px 5px;
+        color: #000000;
+        border-radius: 5px
+      }
+      #mode {
+        background-color: #ff8f88;
+        color: #ffffff;
+      }
+      #scratchpad {
+        background-color: #c4c4c4;
+        border-radius: 0px;
+        color: #000000;
+      }
+      #scratchpad.empty {
+        background-color: transparent;
+        border-radius: 0px;
+        color: #595959;
+      }
+      #window {
+        border-radius: 0px;
+        background-color: #c4c4c4;
+        font-family: Ubuntu;
+        font-size: 16px;
+        font-weight: normal;
+      }
+      .modules-left > widget:first-child > #workspaces {
+        margin-left: 0;
+      }
+      .modules-right > widget:last-child > #workspaces {
+        margin-right: 0;
+      }
+      #battery {
+        color: #000000;
+      }
       #battery.charging {
-         background-color: #000000;
-       }
-
-       @keyframes blink {
-         to {
-           color: #FFFFFF;
-         }
-       }
-
-       #battery.critical:not(.charging) {
-         background: #f53c3c;
-         animation-name: blink;
-         animation-duration: 0.5s;
-         animation-timing-function: linear;
-         animation-iteration-count: infinite;
-         animation-direction: alternate;
-       }
-
-       #idle_inhibitor.activated {
-         background-color: #FFFFFF;
-         color: #000000;
-       }
+        background-color: #ffffff;
+        color: #000000;
+      }
+      @keyframes blink {
+        to {
+          background-color: #ffffff;
+          color: #eee8d5;
+        }
+      }
+      #battery.critical:not(.charging) {
+        background-color: #ff8f88;
+        color: #ffffff;
+        animation-name: blink;
+        animation-duration: 0.5s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+      }
+      label:focus {
+        background-color: #595959;
+      }
+      #cpu {
+        color: #000000;
+      }
+      #memory {
+        color: #000000;
+      }
+      #backlight {
+        color: #000000;
+      }
+      #network {
+        color: #000000;
+      }
+      #network.disconnected {
+        color: #ffffff;
+        background-color: #ff8f88;
+      }
+      #pulseaudio {
+        color: #000000;
+      #pulseaudio.muted {
+        color: #5e5C64;
+      }
+      #temperature {
+        color: #000000;
+      }
+      #temperature.critical {
+        color: #ffffff;
+        background-color: #ff8f88;
+      }
+      #tray {
+        background-color: #000000;
+      }
+      #idle_inhibitor {
+        color: #595959;
+      }
+      #idle_inhibitor.activated {
+        color: #000000;
+        margin: 0px 5px;
+        box-shadow: inset 0px 0px #ecF0F1;
+      }
+      #mpd {
+        color: #000000;
+      }
+      #mpd.stopped {
+        color: #595959;
+      }
     '';
   };
 }
