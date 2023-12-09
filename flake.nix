fda594faaa13ab1a2b4e22f326231280e4310fa7
Dominic Egginton <dominic.egginton@gmail.com>
2023-12-09 12:18:35 +0000

    refactor(workspace): move default dev shell to flake.nix

--- flake.nix
+++ flake.nix
@@ -86,16 +86,31 @@
             outputs.overlays.unstable-packages
           ];
         };
-      in
-        pkgs.callPackage ./shell.nix {
-          inherit pkgs;
+      in {
+        default = pkgs.mkShell rec {
           inherit
             (inputs.sops-nix.packages."${system}")
             sops-import-keys-hook
             ssh-to-pgp
             sops-init-gpg-key
             ;
-        }
+          NIX_CONFIG = "experimental-features = nix-command flakes";
+          sopsPGPKeyDirs = ["./secrets/keys"];
+          nativeBuildInputs = with pkgs; [
+            nix
+            home-manager
+            ssh-to-pgp
+            sops
+            sops-import-keys-hook
+            ssh-to-pgp
+            sops-init-gpg-key
+            workspace.rebuild-host
+            workspace.rebuild-home
+            workspace.rebuild-iso-console
+            workspace.gpg-import-keys
+          ];
+        };
+      }
     );
 
     #######################################
