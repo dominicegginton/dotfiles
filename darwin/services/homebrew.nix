1974fb17f13439cd4c62a4205739955e52d7f811
Dominic Egginton <dominic.egginton@gmail.com>
2023-12-14 12:34:24 +0000

    feat(darwin): manage app store application with mas via homebrew

--- darwin/services/homebrew.nix
+++ darwin/services/homebrew.nix
@@ -11,4 +11,16 @@
     "firefox-developer-edition"
     "google-chrome"
   ];
+  homebrew.masApps = {
+    "Afinity Photo" = 824183456;
+    "Afinity Designer" = 824171161;
+    "Amphetamine" = 937984704;
+    "Xcode" = 497799835;
+    "Developer" = 640199958;
+    "Microsoft To Do" = 1274495053;
+    "Resolution Chanager Pro" = 1111438530;
+    "Pasteboard Viewer" = 1499215709;
+    "TestFlight" = 899247664;
+    "Transporter" = 1450874784;
+  };
 }
