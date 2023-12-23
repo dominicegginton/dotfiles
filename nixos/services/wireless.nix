3a1522626592dd0f98a71eaf0644c0c464c49f79
Dominic Egginton <dominic.egginton@gmail.com>
2023-12-23 11:25:24 +0000

    feat(nixos): add burbage wrirless network

--- nixos/services/wireless.nix
+++ nixos/services/wireless.nix
@@ -8,5 +8,6 @@
     fallbackToWPA2 = true;
     environmentFile = config.sops.secrets."wireless.env".path;
     networks."@home_uuid@".psk = "@home_psk@";
+    networks."@burbage_uuid@".psk = "@burbage_psk@";
   };
 }
