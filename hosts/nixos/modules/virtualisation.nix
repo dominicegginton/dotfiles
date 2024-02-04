f376f867d4ab1dd2cb64a7042c1ccfe8e03edd98
Dominic Egginton <dominic.egginton@gmail.com>
2024-02-04 12:54:30 +0000

    feat(nixos): add nixosvmtest user

--- hosts/nixos/modules/virtualisation.nix
+++ hosts/nixos/modules/virtualisation.nix
@@ -1,7 +1,17 @@
 {...}: {
-  virtualisation.vmVariant.virtualisation = {
-    memorySize = 2048;
-    cores = 3;
-    graphics = false;
+  virtualisation.vmVariant = {
+    users.groups.nixosvmtest = {};
+    users.users.nixvmtest = {
+      description = "NixOS Test User";
+      isNormalUser = true;
+      initialPassword = "test";
+      group = "nixosvmtest";
+    };
+
+    virtualisation = {
+      memorySize = 2048;
+      cores = 3;
+      graphics = false;
+    };
   };
 }
