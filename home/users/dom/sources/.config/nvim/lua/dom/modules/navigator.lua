2194eebb58cd1876174d47c1b953235353373fe2
Dominic Egginton <dominic.egginton@gmail.com>
2024-02-13 10:09:42 +0000

    feat(home): update nvim navigator plugin config

--- home/users/dom/sources/.config/nvim/lua/dom/modules/navigator.lua
+++ home/users/dom/sources/.config/nvim/lua/dom/modules/navigator.lua
@@ -1,3 +1,11 @@
 local navigator = require('navigator')
 
-navigator.setup({ border = 'none' })
+navigator.setup({
+  border = 'none',
+  lsp = {
+    disable_lsp = 'all',
+    format_on_save = {
+      enable = { 'lua' },
+    },
+  },
+})
