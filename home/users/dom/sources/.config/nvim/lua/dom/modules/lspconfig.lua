73813a72054487583de3fa8f701cbd2aad003e71
Dominic Egginton <dominic.egginton@gmail.com>
2024-02-16 17:41:26 +0000

    fix(home): ensure navigator keymappings are setup on lsp attach

--- home/users/dom/sources/.config/nvim/lua/dom/modules/lspconfig.lua
+++ home/users/dom/sources/.config/nvim/lua/dom/modules/lspconfig.lua
@@ -1,26 +1,34 @@
 local lspconfig = require('lspconfig')
--- local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
--- local capabilities = cmp_nvim_lsp.default_capabilities()
 local capabilities = lspconfig.util.default_config
--- lsp_defaults.capabilities = vim.tbl_extend('force', lsp_defaults.capabilities, capabilities)
--- capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local function default_on_attach(client, bufnr)
+  local navigator_lspclient_mapping = require('navigator.lspclient.mapping')
+  navigator_lspclient_mapping.setup({ bufnr = bufnr, client = client })
+end
+
+local default_config = { capabilities = capabilities, on_attach = default_on_attach }
 
 local function eslint_on_attach(_, bufnr)
   vim.api.nvim_create_autocmd('BufWritePre', { buffer = bufnr, command = 'EslintFixAll' })
+  default_on_attach(_, bufnr)
 end
 
-lspconfig['vimls'].setup({ capabilities = capabilities })
-lspconfig['bashls'].setup({ capabilities = capabilities })
-lspconfig['yamlls'].setup({ capabilities = capabilities })
-lspconfig['dockerls'].setup({ capabilities = capabilities })
-lspconfig['docker_compose_language_service'].setup({ capabilities = capabilities })
-lspconfig['tsserver'].setup({ capabilities = capabilities })
-lspconfig['eslint'].setup({ capabilities = capabilities, on_attach = eslint_on_attach })
-lspconfig['angularls'].setup({ capabilities = capabilities })
-lspconfig['pyright'].setup({ capabilities = capabilities })
+lspconfig['vimls'].setup(default_config)
+lspconfig['bashls'].setup(default_config)
+lspconfig['yamlls'].setup(default_config)
+lspconfig['dockerls'].setup(default_config)
+lspconfig['docker_compose_language_service'].setup(default_config)
+lspconfig['tsserver'].setup(default_config)
+lspconfig['eslint'].setup({
+  capabilities = capabilities,
+  on_attach = eslint_on_attach,
+})
+lspconfig['angularls'].setup(default_config)
+lspconfig['pyright'].setup(default_config)
 lspconfig['rust_analyzer'].setup({
   capabilities = capabilities,
   ssettings = { ['rust-analyzer'] = { diagnostics = { enable = false } } },
+  on_attach = default_on_attach,
 })
-lspconfig['lua_ls'].setup({ capabilities = capabilities })
+lspconfig['lua_ls'].setup(default_config)
