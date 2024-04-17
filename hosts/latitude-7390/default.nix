3e785f5e653e34e647964c18bedd996ada1af1bd
Dominic Egginton <dominic.egginton@gmail.com>
2024-04-17 15:59:51 +0100

    feat(hosts/latitude-7390): add support for running other arch bins

--- hosts/latitude-7390/default.nix
+++ hosts/latitude-7390/default.nix
@@ -18,6 +18,14 @@
   boot = {
     initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod"];
     kernelModules = ["kvm-intel"];
+    binfmt.emulatedSystems = [
+      "wasm32-wasi"
+      "x86_64-windows"
+      "aarch64-linux"
+      "armv7l-linux"
+      "riscv32-linux"
+      "riscv64-linux"
+    ];
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
