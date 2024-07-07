ef1f770a50f954d440eff85e473eee60b0f8328d
Dominic Egginton <dominic.egginton@gmail.com>
2024-07-07 22:13:07 +0100

    feat(pkgs): update collect garbage script

--- pkgs/collect-garbage.nix
+++ pkgs/collect-garbage.nix
@@ -5,9 +5,20 @@ pkgs.writeShellApplication {
   runtimeInputs = with pkgs; [ nix ];
 
   text = ''
-    nix-env --delete-generations old
-    nix-collect-garbage --delete-old
-    nix-store --optimise
-    nix-store --gc
+    run_cmd_and_sudo() {
+      "$@"
+      sudo "$@"
+    }
+
+    echo "Collecting garbage..."
+    run_cmd_and_sudo nix-collect-garbage
+    echo "Optimizing store..."
+    run_cmd_and_sudo nix-store --optimise
+    echo "Garbage collecting..."
+    run_cmd_and_sudo nix store gc
+    echo "Deleting old generations..."
+    nix-env --delete-generations 2d
+    sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations 2d
+    home-manager expire-generations "-2 days"
   '';
 }
