e8dcd9ce631e41b977c537b21c65185c34be25b4
Dominic Egginton <dominic.egginton@gmail.com>
2024-08-16 12:30:24 +0100

    chore(hosts/ghost-gs60): fix nvidia setup

--- hosts/nixos/ghost-gs60/default.nix
+++ hosts/nixos/ghost-gs60/default.nix
@@ -3,41 +3,34 @@
 {
   imports = [
     inputs.nixos-hardware.nixosModules.msi-gs60
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
     ./disks.nix
     ./boot.nix
   ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      amdvlk
-      intel-media-driver
-      libvdpau-va-gl
-      nvidia-vaapi-driver
-      vaapiIntel
-      vaapiVdpau
-      vulkan-validation-layers
-    ];
-  };
-
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
     prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      sync.enable = false;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:01:00:0";
     };
   };
 
   boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" ];
-    kernelModules = [ "kvm-intel" ];
+    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    kernelModules = [ "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    kernelParams = [ "nvidia-drm.fbdev=1" ];
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
