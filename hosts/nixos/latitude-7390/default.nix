3f574ad12de86aea57973a0a67a5ef7c325c4d28
Dominic Egginton <dominic.egginton@gmail.com>
2024-08-19 09:35:23 +0100

    chore(workspace): fix distributed builds

--- hosts/nixos/latitude-7390/default.nix
+++ hosts/nixos/latitude-7390/default.nix
@@ -19,14 +19,12 @@
     users.dom.enable = true;
     display.enable = true;
     display.plasma.enable = true;
-
     services = {
       distributedBuilds.enable = true;
       virtualisation.enable = true;
       bluetooth.enable = true;
       syncthing.enable = true;
       ssh.enable = true;
-
       networking = {
         enable = true;
         hostname = "latitude-7390";
--- modules/nixos/services/distributed-builds.nix
+++ modules/nixos/services/distributed-builds.nix
@@ -11,31 +11,20 @@ with lib;
 
   config = mkIf cfg.enable {
     nix.distributedBuilds = true;
+    nix.extraOptions = "builders-use-substitutes = true";
     nix.buildMachines = [{
       hostName = "ghost-gs60";
       system = "x86_64-linux";
       protocol = "ssh-ng";
-      # sshKey = config.sops.secrets.ssh-remote-builder.path;
-      maxJobs = 8;
+      maxJobs = 1;
       speedFactor = 2;
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-      ];
-      supportedFeatures = [
-        "big-parallel"
-        "kvm"
-        "nixos-test"
-      ];
+      systems = [ "x86_64-linux" "i686-linux" ];
+      supportedFeatures = [ "big-parallel" "kvm" "nixos-test" ];
     }];
-
-    # programs.ssh.extraConfig = ''
-    #   Host ghost-gs60
-    #     User nix
-    #     ProxyJump login-tum
-    #     HostName ghost-gs60
-    #     IdentityFile ${config.sops.secrets.ssh-remote-builder.path}
-    # '';
-
+    programs.ssh.extraConfig = ''
+      Host ghost-gs60
+        User nixremote
+        IdentityFile /root/.ssh/nixremote
+    '';
   };
 }
