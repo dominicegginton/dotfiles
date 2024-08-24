c5de495d85119026d93efe2ec9b8451fa98cafbf
Dominic Egginton <dominic.egginton@gmail.com>
2024-08-24 10:39:51 +0100

    chore(home): add plasma setting to nix

--- modules/home-manager/display/plasma.nix
+++ modules/home-manager/display/plasma.nix
@@ -8,8 +8,12 @@ with lib;
 
 {
   options.modules.display.plasma.enable = mkEnableOption "Plasma desktop environment";
+  options.modules.display.plasma.shortcuts = mkOption { type = types.attrs; };
+  options.modules.display.plasma.configFile = mkOption { type = types.attrs; };
 
   config = mkIf cfg.enable {
     programs.plasma.enable = true;
+    programs.plasma.shortcuts = cfg.shortcuts;
+    programs.plasma.configFile = cfg.configFile;
   };
 }
