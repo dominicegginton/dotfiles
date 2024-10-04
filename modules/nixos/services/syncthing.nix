7aac012d7f40f414a95e6a781285bbe7aa7cfd44
Dominic Egginton <dominic.egginton@gmail.com>
2024-10-04 16:50:51 +0100

    chore: update

--- modules/nixos/services/syncthing.nix
+++ modules/nixos/services/syncthing.nix
@@ -10,30 +10,19 @@ with lib;
   options.modules.services.syncthing.enable = lib.mkEnableOption "Syncthing";
 
   config = mkIf cfg.enable {
-    sops.secrets."syncthing.latitude-7390.id".neededForUsers = true;
-    sops.secrets."syncthing.ghost-gs60.id".neededForUsers = true;
+    sops.secrets."syncthing.password".neededForUsers = true;
 
     services.syncthing = {
       enable = true;
       user = "dom";
       dataDir = "/home/dom/Documents";
       configDir = "/home/dom/Documents/.config/syncthing";
-      overrideDevices = true;
       overrideFolders = true;
       settings = {
-        devices = {
-          "device1" = { id = config.sops.secrets."syncthing.latitude-7390.id".path; };
-          "device2" = { id = config.sops.secrets."syncthing.ghost-gs60.id".path; };
-        };
         folders = {
           "Documents" = {
             path = "/home/dom/Documents";
-            devices = [ "device1" "device2" ];
           };
-        };
-        gui = {
-          user = "dom";
-          password = "password";
         };
       };
     };
