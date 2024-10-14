37b8c20de322eac84d474ba79fd51101194383a3
Dominic Egginton <dominic.egginton@gmail.com>
2024-10-14 11:27:21 +0100

    chore: update collect garbage script to better work with new nix settings

--- pkgs/collect-garbage.nix
+++ pkgs/collect-garbage.nix
@@ -5,20 +5,10 @@ pkgs.writeShellApplication {
   runtimeInputs = with pkgs; [ nix ];
 
   text = ''
-    run_cmd_and_sudo() {
-      "$@"
-      sudo "$@"
-    }
-
-    echo "Collecting garbage..."
-    run_cmd_and_sudo nix-collect-garbage
-    echo "Optimizing store..."
-    run_cmd_and_sudo nix-store --optimise
-    echo "Garbage collecting..."
-    run_cmd_and_sudo nix store gc
-    echo "Deleting old generations..."
-    nix-env --delete-generations 2d
-    sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations 2d
-    home-manager expire-generations "-2 days"
+    nix store gc --no-keep-derivations --no-keep-outputs --max 20G
+    nix-env --delete-generations +2
+    sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +2
+    home-manager expire-generations "-1 days"
+    nix store optimise
   '';
 }
