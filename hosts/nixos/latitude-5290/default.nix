989489900a4757343f1fc86e15039babaf244520
Dominic Egginton <dominic.egginton@gmail.com>
2025-01-11 10:19:55 +0000

    chore: add hardware config

--- hosts/nixos/latitude-5290/default.nix
+++ hosts/nixos/latitude-5290/default.nix
@@ -1,7 +1,11 @@
-{ lib, ... }:
+{ inputs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = with inputs.nixos-hardware.nixosModules; [
+    ./hardware-configuration.nix
+    common-pc-laptop
+    common-pc-laptop-ssd
+  ];
   disko.devices = {
     disk = {
       my-disk = {
@@ -11,20 +15,25 @@
           type = "gpt";
           partitions = {
             ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "128M";
               type = "EF00";
-              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
+                type = "btrfs";
+                extraArgs = [ "-f" ];
                 mountpoint = "/";
+                mountOptions = [ "noatime" ];
               };
             };
           };
