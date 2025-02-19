2b4d8632a7d50c4b7fa22a24f7dabc4955243de1
Dominic Egginton <dominic.egginton@gmail.com>
2025-02-19 15:37:23 +0000

    chore: update

--- hosts/nixos/ghost-gs60/default.nix
+++ hosts/nixos/ghost-gs60/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   imports = with inputs.nixos-hardware.nixosModules; [
@@ -68,6 +68,23 @@
   hardware.mwProCapture.enable = true;
   hardware.logitech.wireless.enable = true;
   hardware.logitech.wireless.enableGraphical = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  hardware.graphics.extraPackages = with pkgs; [ intel-media-driver intel-ocl intel-vaapi-driver mesa ];
+  hardware.graphics.extraPackages32 = with pkgs.pkgsi686Linux; [ intel-media-driver intel-vaapi-driver ];
+  hardware.nvidia = {
+    open = false;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    prime.offload.enable = true;
+    prime.offload.enableOffloadCmd = true;
+    prime.intelBusId = "PCI:00:02:0";
+    prime.nvidiaBusId = "PCI:01:00:0";
+  };
+  environment.systemPackages = with pkgs; [ nvtopPackages.full ];
   environment.sessionVariables."VK_DRIVER_FILES" = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
   topology.self.hardware.info = "Homelab Server";
   modules = {
