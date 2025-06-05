d6ade727c9634d0a249952e54f5e04d67e61caeb
Dominic Egginton <dominic.egginton@gmail.com>
2025-06-05 09:25:59 +0100

    feat: ms-edit

--- overlays.nix
+++ overlays.nix
@@ -4,18 +4,54 @@ with outputs.lib;
 
 rec {
   unstable = _: prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      inherit (prev) system hostPlatform config;
-      overlays = [ default ];
-    };
-    lib = prev.lib // outputs.lib;
+    unstable = import inputs.nixpkgs-unstable
+      { inherit (prev) system hostPlatform config; overlays = [ default ]; }
+    //
+    { lib = prev.lib // outputs.lib; };
   };
   bleeding = _: prev: {
-    bleeding = import inputs.nixpkgs-bleeding {
-      inherit (prev) system hostPlatform config;
-      overlays = [ default ];
+    bleeding = import inputs.nixpkgs-bleeding
+      { inherit (prev) system hostPlatform config; overlays = [ default ]; }
+    //
+    {
+      lib = prev.lib // outputs.lib;
+      ms-edit = prev.callPackage
+        ({ lib, rustPlatform }:
+          rustPlatform.buildRustPackage rec {
+            pname = "ms-edit";
+            version = "v1.1.0";
+            src = prev.fetchFromGitHub {
+              owner = "microsoft";
+              repo = "edit";
+              tag = version;
+              hash = "sha256-ubdZynQVwCYhZA/z4P/v6aX5rRG9BCcWKih/PzuPSeE=";
+            };
+            cargoHash = "sha256-qT4u8LuKX/QbZojNDoK43cRnxXwMjvEwybeuZIK6DQQ=";
+          })
+        { };
+      bootstrap = with final; writeShellScriptBin "bootstrap" ''
+        export PATH=${lib.makeBinPath [ status ensure-user-is-root ensure-tailscale-is-connected coreutils git busybox nix fzf jq gum bws ]};
+        set -efu -o pipefail
+        ensure-user-is-root
+        ensure-tailscale-is-connected
+        status
+        gum style --foreground '#ff0000' --bold "CURRENTLY IN DEVELOPMENT"
+        gum confirm "Accept warning Are you sure you want to continue?" || {
+          gum log --level error "User cancelled the bootstrap process."
+          exit 0;
+        }
+        temp=$(mktemp -d)
+        cleanup() {
+          gum log --level info "Cleaning up temporary directory $temp."
+          rm -rf "$temp" || true
+        }
+        trap cleanup EXIT
+        git clone --depth 1 https://github.com/dominicegginton/dotfiles "$temp/dotfiles" || {
+          gum log --level error "Failed to clone dotfiles repository."
+          exit 1;
+        }
+      '';
     };
-    lib = prev.lib // outputs.lib;
   };
   default = final: prev: {
     ensure-tailscale-is-connected = final.callPackage ./pkgs/ensure-tailscale-is-connected.nix { };
@@ -36,28 +72,6 @@ rec {
     vscode-with-extensions = import ./pkgs/vscode-with-extensions.nix { inherit (prev) vscode-with-extensions; inherit (final) vscode-extensions; };
     vulnix = final.callPackage (packagesFrom inputs.vulnix).vulnix { };
     lib = prev.lib // outputs.lib;
-    ## WIP
-    bootstrap = with final; writeShellScriptBin "bootstrap" ''
-      export PATH=${lib.makeBinPath [ status ensure-user-is-root ensure-tailscale-is-connected coreutils git busybox nix fzf jq gum bws ]};
-      set -efu -o pipefail
-      ensure-user-is-root
-      ensure-tailscale-is-connected
-      status
-      gum style --foreground '#ff0000' --bold "CURRENTLY IN DEVELOPMENT"
-      gum confirm "Accept warning Are you sure you want to continue?" || {
-        gum log --level error "User cancelled the bootstrap process."
-        exit 0;
-      }
-      temp=$(mktemp -d)
-      cleanup() {
-        gum log --level info "Cleaning up temporary directory $temp."
-        rm -rf "$temp" || true
-      }
-      trap cleanup EXIT
-      git clone --depth 1 https://github.com/dominicegginton/dotfiles "$temp/dotfiles" || {
-        gum log --level error "Failed to clone dotfiles repository."
-        exit 1;
-      }
-    '';
+
   };
 }
