99cccbe8289fe65b7b26aca0d540da8583f3f131
Dominic Egginton <dominic.egginton@gmail.com>
2025-07-11 11:20:23 +0100

    chore: update backup module

--- modules/nixos/backup.nix
+++ modules/nixos/backup.nix
@@ -9,6 +9,12 @@ with pkgs.writers;
     let
       backupConfigType = submodule {
         options = {
+          project = mkOption {
+            type = str;
+            default = "dominicegginton-personal";
+            description = "Google Cloud project to use for backups.";
+            example = "dominicegginton-personal";
+          };
           bucket = mkOption {
             type = str;
             default = "";
@@ -33,6 +39,12 @@ with pkgs.writers;
             description = "Systemd timer configuration for the backup job.";
             example = "1d";
           };
+          mirror = mkOption {
+            type = bool;
+            default = false;
+            description = "Always mirror the source directory to the destination.";
+            example = true;
+          };
         };
       };
     in
@@ -42,7 +54,7 @@ with pkgs.writers;
       description = "Configuration for backup jobs.";
       example = {
         exampleJob = {
-          bucket = "gcs://my-backup-bucket";
+          bucket = "gs://my-backup-bucket";
           from = "/var/lib/myapp";
           to = "myapp-backups";
           schedule = "2m";
@@ -53,27 +65,39 @@ with pkgs.writers;
     systemd.timers = mapAttrs
       (
         name: { schedule, ... }: {
-          description = "Backup timer for gcp-backup-${name}";
+          description = "Backup timer for ${name}";
           wantedBy = [ "timers.target" ];
           timerConfig = {
-            OnBootSec = "0m";
+            OnBootSec = "5min";
             OnUnitActiveSec = schedule;
-            Unit = "gcp-backup-${name}.service";
+            Unit = "${name}.service";
           };
         }
       )
       config.backup;
     systemd.services = mapAttrs
       (
-        name: { bucket, from, to, ... }: {
+        name: { project, bucket, from, to, mirror, ... }: {
           description = "Backup service for ${name}";
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" "secrets.target" ];
           serviceConfig.type = "oneshot";
           serviceConfig.remainAfterExit = true;
           script = ''
-            export PATH=${makeBinPath [ pkgs.gum pkgs.gcsfuse ]}:$PATH
-            gum log --level error "NOT IMPLEMENTED: Backup from ${from} to ${bucket}/${hostname}/${to}"
+            export PATH=${makeBinPath [ pkgs.gum pkgs.google-cloud-sdk pkgs.rsync ]}:$PATH;
+            set -e
+            if ! gcloud auth list --filter=status:ACTIVE --format="value(account)" | grep -q .; then
+              gum log --level error "You are not authenticated with Google Cloud. Please run 'gcloud auth login' to authenticate."
+              exit 1;
+            fi 
+            gcloud config set project "${project}" || {
+              gum log --level error "Failed to set Google Cloud project. Please run 'gcloud config set project <your-project>' manually."
+              exit 1;
+            }
+            gum log --level info "Starting backup from ${from} to ${bucket}/${to}..."
+            gsutil -m rsync ${lib.optionalString mirror "-d"} -r "${from}" "${bucket}/${to}" || { 
+              gum log --level error "Backkup failed."
+              exit 1;
+            }
+            gum log --level info "Backup completed successfully."
           '';
         }
       )
