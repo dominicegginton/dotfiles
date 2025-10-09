419d9525f8a72f159dca806506c2afcd6beb88d4
Dominic Egginton <dominic.egginton@gmail.com>
2025-10-09 16:30:37 +0100

    chore: tidy

--- modules/nixos/display/steamos.nix
+++ modules/nixos/display/steamos.nix
@@ -1,5 +1,5 @@
 { config, ... }: {
-    options.display.steamos.enable.default = false;
+  options.display.steamos.enable.default = false;
 
-    config = lib.mkIf config.display.steamos.enable { };
+  config = lib.mkIf config.display.steamos.enable { };
 }
