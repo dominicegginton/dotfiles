2ebc9e4b913c84fd3af3c947dcc3981713021eaf
Dominic Egginton <dominic.egginton@gmail.com>
2025-10-29 07:22:38 +0000

    feat: set disk devices

--- hosts/nixos/ghost-gs60.nix
+++ hosts/nixos/ghost-gs60.nix
@@ -10,7 +10,7 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/sda";
+        device = "/dev/sdb";
         type = "disk";
         content = {
           type = "gpt";
@@ -41,7 +41,7 @@
         };
       };
       extra = {
-        device = "/dev/sdb";
+        device = "/dev/sda";
         type = "disk";
         content = {
           type = "gpt";
