4fc484b706c13c8c406b0b2609a1a7a7785802fc
Dominic Egginton <dominic.egginton@gmail.com>
2025-11-07 19:22:33 +0000

    chore: update

--- hosts/latitude-7390.nix
+++ hosts/latitude-7390.nix
@@ -16,8 +16,6 @@
   boot.kernelModules = [ "kvm-intel" "vhost_vsock" "i2c-dev" "ddcci_backlight" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
 
-  display.niri.enable = true;
-
   boot.kernel.sysctl = {
     # Set swappiness to only swap if you really have to
     # The sysctl swappiness parameter determines the kernel's preference for pushing anonymous pages or page cache to disk in memory-starved situations.
@@ -26,7 +24,7 @@
     "vm.swappiness" = 1;
 
     # Adjust cache pressure
-    # The value controls the tendency of the kernel to reclaim the memory which is used for caching of directory and inode objects (VFS cache). 
+    # The value controls the tendency of the kernel to reclaim the memory which is used for caching of directory and inode objects (VFS cache).
     # Lowering it from the default value of 100 makes the kernel less inclined to reclaim VFS cache (do not set it to 0, this may produce out-of-memory conditions)
     "vm.vfs_cache_pressure" = 30;
 
@@ -41,12 +39,12 @@
     # dirty data.
     "vm.dirty_background_ratio" = 5;
 
-    # The kernel flusher threads will periodically wake up and write old data out to disk.  
+    # The kernel flusher threads will periodically wake up and write old data out to disk.
     # This tunable expresses the interval between those wakeups, in 100'ths of a second (Default is 500).
     "vm.dirty_writeback_centisecs" = 1000; # 10 seconds
 
-    # page-cluster controls the number of pages up to which consecutive pages are read in from swap in a single attempt. 
-    # This is the swap counterpart to page cache readahead. The mentioned consecutivity is not in terms of virtual/physical addresses, 
+    # page-cluster controls the number of pages up to which consecutive pages are read in from swap in a single attempt.
+    # This is the swap counterpart to page cache readahead. The mentioned consecutivity is not in terms of virtual/physical addresses,
     # but consecutive on swap space - that means they were swapped out together. (Default is 3)
     # increase this value to 1 or 2 if you are using physical swap (1 if ssd, 2 if hdd)
     "vm.page-cluster" = 0; # Set to 0 for SSD
@@ -90,7 +88,7 @@
     # Adjust the console log level to suppress messages.
     "kernel.printk" = "3 3 3 3";
 
-    # Disable Kexec, which allows replacing the current running kernel. 
+    # Disable Kexec, which allows replacing the current running kernel.
     # Enhances security by preventing kernel replacement.
     "kernel.kexec_load_disabled" = 1;
 
