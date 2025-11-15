131171145f8cbf561f17ef56980e98a2900cfec4
Dominic Egginton <dominic.egginton@gmail.com>
2025-11-15 07:47:05 +0000

    chore: update

--- hosts/latitude-7390.nix
+++ hosts/latitude-7390.nix
@@ -22,109 +22,65 @@
     # A low value causes the kernel to prefer freeing up open files (page cache), a high value causes the kernel to try to use swap space,
     # and a value of 100 means IO cost is assumed to be equal.
     "vm.swappiness" = 1;
-
     # Adjust cache pressure
     # The value controls the tendency of the kernel to reclaim the memory which is used for caching of directory and inode objects (VFS cache).
     # Lowering it from the default value of 100 makes the kernel less inclined to reclaim VFS cache (do not set it to 0, this may produce out-of-memory conditions)
     "vm.vfs_cache_pressure" = 30;
-
     # Contains, as a bytes of total available memory that contains free pages and reclaimable
     # pages, the number of pages at which a process which is generating disk writes will itself start
     # writing out dirty data.
     "vm.dirty_bytes" = 1073741824; # 1 GB
-
     # Optimize storage related settings
     # Contains, as a bytes of total available memory that contains free pages and reclaimable
     # pages, the number of pages at which the background kernel flusher threads will start writing out
     # dirty data.
     "vm.dirty_background_ratio" = 5;
-
     # The kernel flusher threads will periodically wake up and write old data out to disk.
     # This tunable expresses the interval between those wakeups, in 100'ths of a second (Default is 500).
     "vm.dirty_writeback_centisecs" = 1000; # 10 seconds
-
     # page-cluster controls the number of pages up to which consecutive pages are read in from swap in a single attempt.
     # This is the swap counterpart to page cache readahead. The mentioned consecutivity is not in terms of virtual/physical addresses,
     # but consecutive on swap space - that means they were swapped out together. (Default is 3)
     # increase this value to 1 or 2 if you are using physical swap (1 if ssd, 2 if hdd)
     "vm.page-cluster" = 0; # Set to 0 for SSD
-
-    # Allow unprivileged users to bind ports below 1024
+    # allow unprivileged users to bind ports below 1024
     "net.ipv4.ip_unprivileged_port_start" = 0;
-
     # Increase max_user_watches
     # Defines the maximum number of file watches that can be registered.
     "fs.inotify.max_user_watches" = 1048576;
-
     # Set size of file handles and inode cache
     # Sets the maximum number of file descriptors that can be allocated.
     "fs.file-max" = 4194304;
-
     # Increase writeback interval for xfs
     # Defines the interval (in centiseconds) at which the XFS kernel threads flush data to disk.
     "fs.xfs.xfssyncd_centisecs" = 10000; # 100 seconds
-
     # Defines the maximum number of asynchronous I/O requests that can be in progress at a given time.
     "fs.aio-max-nr" = 1000000; # 1 million
-
     # Disable core dumps
     # Redirect core dumps to /dev/null to disable them.
     "kernel.core_pattern" = "/dev/null";
-
     # Increase the sched_rt_runtime_us to mitigate issues:
     # sched: RT throttling activated
     # Defines the time (in microseconds) that real-time tasks can run without being throttled.
     "kernel.sched_rt_runtime_us" = 980000;
-
     # This action will speed up your boot and shutdown, because one less module is loaded.
     # Additionally, disabling watchdog timers increases performance and lowers power consumption.
     # Disable NMI watchdog
     "kernel.nmi_watchdog" = 0;
-
-    # Enable the sysctl setting kernel.unprivileged_userns_clone to allow normal users to run unprivileged containers.
+    # enable the sysctl setting kernel.unprivileged_userns_clone to allow normal users to run unprivileged containers.
     "kernel.unprivileged_userns_clone" = 1;
-
-    # Hide any kernel messages from the console
+    # hide any kernel messages from the console
     # Adjust the console log level to suppress messages.
     "kernel.printk" = "3 3 3 3";
-
-    # Disable Kexec, which allows replacing the current running kernel.
-    # Enhances security by preventing kernel replacement.
+    # disable Kexec, which allows replacing the current running kernel.
+    # enhance security by preventing kernel replacement.
     "kernel.kexec_load_disabled" = 1;
-
     # enable SysRQ for rebooting the machine properly if it freezes.
     # useful for emergency recovery.
     "kernel.sysrq" = 1;
-
     # allows a large number of processes and threads to be managed.
     # sets the maximum number of process IDs that can be allocated.
     "kernel.pid_max" = 262144;
-
-    # increase the maximum connections
-    # ihe upper limit on how many connections the kernel will accept (default 4096 since kernel version 5.6):
-    "net.core.somaxconn" = 8192;
-    # help prevent packet loss during high traffic periods.
-    # defines the maximum number of packets that can be queued on the network device input queue.
-    "net.core.netdev_max_backlog" = 65536;
-    # default socket receive buffer size, improve network performance & applications that use sockets. adjusted for 8GB RAM.
-    "net.core.rmem_default" = 1048576; # 1 MB
-    # maximum socket receive buffer size, determine the amount of data that can be buffered in memory for network operations - adjusted for 8GB RAM.
-    "net.core.rmem_max" = 67108864; # 64 MB
-    # default socket send buffer size, improve network performance & applications that use sockets. Adjusted for 8GB RAM.
-    "net.core.wmem_default" = 1048576; # 1 MB
-    # maximum socket send buffer size - ajusted for 8GB RAM.
-    "net.core.wmem_max" = 67108864; # 64 MB
-    # reduce the chances of fragmentation
-    "net.ipv4.ipfrag_high_threshold" = 5242880; # 5 MB
-    # allow tcp window size to grow beyond its default maximum value.
-    "net.ipv4.tcp_window_scaling" = 1;
-    # define the memory reserved for TCP read operations.
-    "net.ipv4.tcp_rmem" = "4096 87380 67108864";
-    # define the memory reserved for tcp write operations
-    "net.ipv4.tcp_wmem" = "4096 65536 67108864";
-    # optimizes for low latency and high throughput
-    "net.ipv4.tcp_congestion_control" = "bbr";
-
     # increases the frequency at which the kernel's scheduler runs, which can improve system responsiveness.
     "kernel.hz" = 1000;
     # split lock mitigate can slow down performance in some applications.
