d119979fef32eef41bb59333f568a66244608f03
Dominic Egginton <dominic.egginton@gmail.com>
2025-12-04 17:26:19 +0000

    feat: set experimental mutter features

--- modules/display/gnome.nix
+++ modules/display/gnome.nix
@@ -19,6 +19,19 @@
       };
     };
     programs.firefox.enable = true;
+    programs.dconf.profiles.user.databases = [
+      {
+        settings = {
+          "org/gnome/mutter" = {
+            experimental-features = [
+              "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+              "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+              "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+            ];
+          };
+        };
+      }
+    ];
     environment = {
       gnome.excludePackages = with pkgs; [
         gnome-tour
