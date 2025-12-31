0d1b8cce2cf88b944a05d22a5b36bfa80a10be3c
Dominic Egginton <dominic.egginton@gmail.com>
2025-12-31 21:32:13 +0000

    chore: update

--- modules/users/root.nix
+++ modules/users/root.nix
@@ -3,21 +3,15 @@
 with lib;
 
 {
-  config = {
-    users = {
-      users = {
-        root = {
-          enable = lib.mkDefault true;
-          description = "System administrator";
-          isNormalUser = lib.mkDefault false;
-          isSystemUser = lib.mkDefault true;
-          hashedPassword = lib.mkDefault null;
-          openssh = {
-            authorizedPrincipals = [ "root@localhost" "root@${hostname}" self.outputs.lib.maintainers.dominicegginton.email ];
-            authorizedKeys.keys = self.outputs.lib.maintainers.dominicegginton.sshKeys;
-          };
-        };
-      };
+  config.users.users.root = {
+    enable = lib.mkDefault true;
+    description = "System administrator";
+    isNormalUser = lib.mkDefault false;
+    isSystemUser = lib.mkDefault true;
+    hashedPassword = lib.mkDefault null;
+    openssh = {
+      authorizedPrincipals = [ "root@localhost" "root@${hostname}" self.outputs.lib.maintainers.dominicegginton.email ];
+      authorizedKeys.keys = self.outputs.lib.maintainers.dominicegginton.sshKeys;
     };
   };
 }
