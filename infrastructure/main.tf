9caea2d1920fe7e3b4defc417d6d35d9fb4b5d24
Dominic Egginton <dominic.egginton@gmail.com>
2026-01-08 21:16:30 +0000

    chore: update

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -181,18 +181,21 @@ resource "google_project_iam_member" "github_actions_service_account_admin" {
 }
 
 resource "github_actions_secret" "gcp_workload_identity_provider" {
+  count           = var.github_token != null ? 1 : 0
   repository      = "dotfiles"
   secret_name     = "GCP_WORKLOAD_IDENTITY_PROVIDER"
   plaintext_value = google_iam_workload_identity_pool_provider.github.name
 }
 
 resource "github_actions_secret" "gcp_service_account" {
+  count           = var.github_token != null ? 1 : 0
   repository      = "dotfiles"
   secret_name     = "GCP_SERVICE_ACCOUNT"
   plaintext_value = google_service_account.github_actions.email
 }
 
 resource "github_actions_secret" "gcp_project_id" {
+  count           = var.github_token != null ? 1 : 0
   repository      = "dotfiles"
   secret_name     = "GCP_PROJECT_ID"
   plaintext_value = google_project_service.iam.project 
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,7 +4,8 @@ variable "gcp_project_id" {
 }
 
 variable "github_token" {
-  description = "GitHub Personal Access Token for managing repository secrets"
+  description = "GitHub Personal Access Token for managing repository secrets (only needed for local runs)"
   type        = string
   sensitive   = true
+  default     = null
 }
