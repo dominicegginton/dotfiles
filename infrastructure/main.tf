608fd84e2b02f7df97068c347efef4f169218bcc
Dominic Egginton <dominic.egginton@gmail.com>
2026-01-08 19:42:59 +0000

    chore: openid

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,17 +3,37 @@ terraform {
     tailscale = {
       source = "tailscale/tailscale"
     }
+
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.0.0"
+    }
+
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
   }
 }
 
 provider "google" {
-  project = "dominicegginton-personal"
+  project = var.gcp_project_id
   region  = "europe-west2"
 }
 
-provider "tailscale" {}
+provider "github" {
+  owner = "dominicegginton"
+}
 
-provider "random" {}
+resource "google_project_service" "iam" {
+  project = var.gcp_project_id
+  service = "iam.googleapis.com"
+}
 
 resource "random_id" "terraform-remote-backend" {
   byte_length = 8
@@ -85,4 +105,76 @@ resource "google_storage_bucket" "frigate_data" {
   versioning {
     enabled = true
   }
+}
+
+resource "google_project_service" "iamcredentials" {
+  project = var.gcp_project_id
+  service = "iamcredentials.googleapis.com"
+}
+
+resource "google_iam_workload_identity_pool" "github" {
+  project                   = var.gcp_project_id
+  workload_identity_pool_id = "github-actions"
+  display_name              = "GitHub Actions Pool"
+  description               = "Workload Identity Pool for GitHub Actions"
+
+  depends_on = [google_project_service.iamcredentials]
+}
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  project                            = var.gcp_project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions-provider"
+  display_name                       = "GitHub Actions Provider"
+  description                        = "OIDC identity pool provider for GitHub Actions"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+    "attribute.owner"      = "assertion.repository_owner"
+  }
+
+  attribute_condition = "assertion.repository_owner == 'dominicegginton'"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account" "github_actions" {
+  project      = var.gcp_project_id
+  account_id   = "github-actions"
+  display_name = "GitHub Actions Service Account"
+  description  = "Service account for GitHub Actions workflows"
+}
+
+resource "google_service_account_iam_member" "github_actions_workload_identity" {
+  service_account_id = google_service_account.github_actions.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.owner/dominicegginton"
+}
+
+resource "google_project_iam_member" "github_actions_storage_admin" {
+  project = var.gcp_project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+resource "github_actions_secret" "gcp_workload_identity_provider" {
+  repository      = "dotfiles"
+  secret_name     = "GCP_WORKLOAD_IDENTITY_PROVIDER"
+  plaintext_value = google_iam_workload_identity_pool_provider.github.name
+}
+
+resource "github_actions_secret" "gcp_service_account" {
+  repository      = "dotfiles"
+  secret_name     = "GCP_SERVICE_ACCOUNT"
+  plaintext_value = google_service_account.github_actions.email
+}
+
+resource "github_actions_secret" "gcp_project_id" {
+  repository      = "dotfiles"
+  secret_name     = "GCP_PROJECT_ID"
+  plaintext_value = var.gcp_project_id
 }
---
+++ infrastructure/variables.tf
@@ -0,0 +1,4 @@
+variable "gcp_project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
