c8732e7e6c9e491d3276fcfb986516f4c6162df1
Dominic Egginton <dominic.egginton@gmail.com>
2026-01-15 08:24:01 +0000

    chore: update

--- modules/programs/dconf.nix
+++ modules/programs/dconf.nix
@@ -1,7 +1,6 @@
 { lib, ... }:
 
 {
-  # https://stigui.com/stigs/Anduril_NixOS_STIG/groups/V-268086
   programs.dconf.profiles.user.databases = with lib.gvariant; [
     {
       settings."org/gnome/desktop/session".idle-delay = mkUint32 600;
