9b9cb8168336ab24a55de3737943ee8a2139c2e2
Dominic Egginton <dominic.egginton@gmail.com>
2026-01-18 10:45:08 +0000

    chore: tidyup

--- pkgs/youtube.nix
+++ pkgs/youtube.nix
@@ -1,4 +1,6 @@
 { fetchurl
+, stdenv
+, writeText
 , google-chrome
 , wayland-utils
 , wlr-randr
@@ -12,46 +14,45 @@
 
 let
   name = "youtube-via-google-chrome";
+  url = "https://www.youtube.com/tv";
+
   desktopItem = makeDesktopItem {
     inherit name;
     exec = name;
+    desktopName = "Youtube via Google Chrome";
+    genericName = "A video social media and online video sharing platform";
+    categories = [ "TV" "AudioVideo" ];
+    startupNotify = true;
     icon = fetchurl {
       name = "YouTube_full_color_icon_2017.svg";
       url = "https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg";
       sha256 = "sha256-fROAuewbDLM/ZhsgM9E77KfETCxAiabRTElVX/4/Ir8=";
       meta.license = lib.licenses.unfree;
     };
-    desktopName = "Youtube via Google Chrome";
-    genericName = "A video social media and online video sharing platform";
-    categories = [ "TV" "AudioVideo" ];
-    startupNotify = true;
   };
+
   script = writeScriptBin name ''
     #!${runtimeShell}
 
-    if command -v ${wlr-randr}/bin/wlr-randr >/dev/null; then
-      screen_size=$(${wlr-randr}/bin/wlr-randr | awk '/current mode/ {print $3 "x" $4; exit}')
-    elif command -v ${wayland-utils}/bin/wayland-info >/dev/null; then
-      screen_size=$(${wayland-utils}/bin/wayland-info | awk '/current_mode/ {print $2 "x" $3; exit}')
-    else
-      screen_size="1920x1080"
-    fi
-
-    width=$screen_size%x*
-    height=$screen_size%y*
-
-    exec ${google-chrome}/bin/${google-chrome.meta.mainProgram} \
+    # launch chrome with the unpacked extension and wait, then cleanup
+    "${google-chrome}/bin/${google-chrome.meta.mainProgram}" \
       ${lib.escapeShellArgs commandLineArgs} \
-      --app=https://youtube.com/tv \
+      --app=${url} \
+      --new-window \
       --user-agent="Mozilla/5.0 (Linux; Android 9; BRAVIA 4K GB) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36" \
-      --window-size=$width,$height \
+      --window-size=4096,2160 \
       --force-device-scale-factor=1 \
-      --touch-events=enabled \
       --start-fullscreen \
+      --touch-events=enabled \
       --no-first-run \
       --no-default-browser-check \
       --no-crash-upload \
-      "$@"
+      -force-dev-mode-highlighting
+      "$@" &
+
+    CHROME_PID=$!
+    wait $CHROME_PID
+    exit $?
   '';
 in
 
@@ -65,8 +66,7 @@ symlinkJoin {
 
       This package installs an application launcher item that opens YouTube in a dedicated Google Chrome window. If your preferred browser doesn't support YouTube's DRM, this package provides a quick and easy way to launch YouTube on a supported browser, without polluting your application list with a redundant, single-purpose browser.
     '';
-    homepage = google-chrome.meta.homepage or null;
-    license = google-chrome.meta.license;
+    homepage = url;
     maintainers = [ lib.maintainers.dominicegginton ];
     platforms = google-chrome.meta.platforms or lib.platforms.all;
   };
