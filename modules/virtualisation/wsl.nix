dd77765878748550534548821896954c10217c8d
Dominic Egginton <dominic.egginton@gmail.com>
2026-05-23 08:54:17 +0100

    chore: update

--- modules/virtualisation/wsl.nix
+++ modules/virtualisation/wsl.nix
@@ -17,6 +17,12 @@
 
     environment.systemPackages = with pkgs; [ jetbrains.gateway ];
 
+    networking = {
+      nftables.enable = lib.mkForce false; # Disable nftables in WSL
+      networkmanager.enable = lib.mkForce false; # Disable NetworkManager in WSL
+      firewall.enable = lib.mkForce false; # Disable firewall in WSL since Windows handles it
+    };
+
     # Enable nix-ld to run unpatched Linux binaries
     programs.nix-ld.enable = lib.mkForce true;
 
