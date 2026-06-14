93a46286b99334db20052dced46cdde652c41d52
Dominic Egginton <dominic.egginton@gmail.com>
2026-06-14 19:13:33 +0100

    fix: allow hardwired devices

--- modules/services/usbguard.nix
+++ modules/services/usbguard.nix
@@ -1,11 +1,25 @@
 { lib, config, ... }:
 
 let
+  cfg = config.services.usbguard;
   notWSL = !config.wsl.enable;
 in
 
 {
-  services.usbguard = lib.mkIf notWSL {
+  options.services.usbguard.extraRules = lib.mkOption {
+    type = lib.types.lines;
+    default = "";
+    description = ''
+      Extra rules to append to the default USBGuard policy.
+
+      To discover USB devices to whitelist:
+      1. Run `run0 usbguard list-devices`
+      2. Identify the device and its attributes (ID, serial, hash, etc.)
+      3. Add a rule to this option, e.g., `allow id 1234:abcd`
+    '';
+  };
+
+  config.services.usbguard = lib.mkIf notWSL {
     enable = lib.mkDefault true;
     presentControllerPolicy = lib.mkDefault "allow";
     dbus.enable = lib.mkDefault true;
@@ -13,9 +27,13 @@ in
       "usbguard"
       "wheel"
     ];
-    # Allow Bluetooth devices (Class e0:01:01)
-    rules = ''
-      allow with-interface equals { e0:01:01 }
-    '';
+    # Allow Bluetooth and internal (hardwired) devices
+    rules = lib.mkDefault (
+      ''
+        allow with-interface e0:01:01
+        allow with-connect-type "hardwired"
+      ''
+      + cfg.extraRules
+    );
   };
 }
