43ae8d3a5755310a33d81229700b746e27e1badf
Dominic Egginton <dominic.egginton@gmail.com>
2026-07-12 08:52:17 +0100

    feat: tailscale setting and acl policy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,12 +9,47 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.0.0"
     }
+
+    tailscale = {
+      source  = "tailscale/tailscale"
+      version = ">= 0.17.0"
+    }
   }
 }
 
 provider "google" {
   project = var.gcp_project_id # GCP project ID
   region  = "europe-west2"     # GCP region
+}
+
+provider "tailscale" {
+  api_key = var.tailscale_api_key
+  tailnet = var.tailscale_tailnet
+}
+
+resource "tailscale_tailnet_settings" "sample_tailnet_settings" {
+  acls_externally_managed_on                  = false
+  devices_approval_on                         = true
+  devices_auto_updates_on                     = true
+  devices_key_duration_days                   = 5
+  users_approval_on                           = true
+  users_role_allowed_to_join_external_tailnet = "member"
+  https_enabled                               = true
+}
+
+resource "tailscale_acl" "acl" {
+  acl                        = file("${path.module}/tailscale_acl.json")
+  overwrite_existing_content = true
+}
+
+resource "tailscale_dns_configuration" "sample_configuration" {
+  nameservers {
+    address            = "2a07:a8c0::cd:dfb8"
+    use_with_exit_node = true
+  }
+  search_paths       = []
+  override_local_dns = true
+  magic_dns          = true
 }
 
 module "gcp_infrastructure" {
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,16 @@ variable "gcp_project_id" {
   description = "GCP Project ID"
   type        = string
 }
+
+variable "tailscale_api_key" {
+  description = "The API key for Tailscale"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+
+variable "tailscale_tailnet" {
+  description = "The Tailscale tailnet to connect to"
+  type        = string
+  default     = null
+}
