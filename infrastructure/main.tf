17ccbbb26db7585c336296a25510ddf5ad20b503
Dominic Egginton <dominic.egginton@gmail.com>
2026-07-13 21:01:22 +0100

    feat: devices

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,6 +27,20 @@ provider "tailscale" {
   tailnet = var.tailscale_tailnet
 }
 
+resource "tailscale_contacts" "sample_contacts" {
+  account {
+    email = "dominic.egginton@gmail.com"
+  }
+
+  support {
+    email = "dominic.egginton@gmail.com"
+  }
+
+  security {
+    email = "dominic.egginton@gmail.com"
+  }
+}
+
 resource "tailscale_tailnet_settings" "sample_tailnet_settings" {
   acls_externally_managed_on                  = false
   devices_approval_on                         = true
@@ -37,11 +51,6 @@ resource "tailscale_tailnet_settings" "sample_tailnet_settings" {
   https_enabled                               = true
 }
 
-resource "tailscale_acl" "acl" {
-  acl                        = file("${path.module}/tailscale_acl.json")
-  overwrite_existing_content = true
-}
-
 resource "tailscale_dns_configuration" "sample_configuration" {
   nameservers {
     address            = "2a07:a8c0::cd:dfb8"
@@ -50,6 +59,171 @@ resource "tailscale_dns_configuration" "sample_configuration" {
   search_paths       = []
   override_local_dns = true
   magic_dns          = true
+}
+
+resource "tailscale_acl" "acl" {
+  acl                        = file("${path.root}/tailscale_acl.json")
+  overwrite_existing_content = true
+}
+
+data "tailscale_users" "all-users" {}
+
+data "tailscale_device" "latitude-7390" {
+  name = "latitude-7390.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "latitude-7390" {
+  device_id  = data.tailscale_device.latitude-7390.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "latitude-7390" {
+  device_id = data.tailscale_device.latitude-7390.node_id
+  tags      = ["tag:device-latitude-7390"]
+}
+
+data "tailscale_device" "ghost-gs60" {
+  name = "ghost-gs60.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "ghost-gs60" {
+  device_id  = data.tailscale_device.ghost-gs60.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "ghost-gs60" {
+  device_id = data.tailscale_device.ghost-gs60.node_id
+  tags      = ["tag:device-ghost-gs60"]
+}
+
+data "tailscale_device" "steamdeck" {
+  name = "steamdeck.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "steamdeck" {
+  device_id  = data.tailscale_device.steamdeck.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "steamdeck" {
+  device_id = data.tailscale_device.steamdeck.node_id
+  tags      = ["tag:device-steamdeck"]
+}
+
+
+data "tailscale_device" "doms-pixel-9" {
+  name     = "doms-pixel-9.soay-puffin.ts.net"
+  wait_for = "10s"
+}
+
+resource "tailscale_device_authorization" "doms-pixel-9" {
+  device_id  = data.tailscale_device.doms-pixel-9.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "doms-pixel-9" {
+  device_id = data.tailscale_device.doms-pixel-9.node_id
+  tags      = []
+}
+
+data "tailscale_device" "beszel" {
+  name     = "beszel.soay-puffin.ts.net"
+  wait_for = "10s"
+}
+
+resource "tailscale_device_authorization" "beszel" {
+  device_id  = data.tailscale_device.beszel.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "beszel" {
+  device_id = data.tailscale_device.beszel.node_id
+  tags      = ["tag:service-beszel"]
+}
+
+data "tailscale_device" "frigate" {
+  name     = "frigate.soay-puffin.ts.net"
+  wait_for = "10s"
+}
+
+resource "tailscale_device_authorization" "frigate" {
+  device_id  = data.tailscale_device.frigate.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "frigate" {
+  device_id = data.tailscale_device.frigate.node_id
+  tags      = ["tag:service-frigate"]
+}
+
+data "tailscale_device" "idp" {
+  name = "idp.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "idp" {
+  device_id  = data.tailscale_device.idp.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "idp" {
+  device_id = data.tailscale_device.idp.node_id
+  tags      = ["tag:service-idp"]
+}
+
+data "tailscale_device" "immich" {
+  name = "immich.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "immich" {
+  device_id  = data.tailscale_device.immich.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "immich" {
+  device_id = data.tailscale_device.immich.node_id
+  tags      = ["tag:service-immich"]
+}
+
+data "tailscale_device" "jellyfin" {
+  name = "jellyfin.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "jellyfin" {
+  device_id  = data.tailscale_device.jellyfin.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "jellyfin" {
+  device_id = data.tailscale_device.jellyfin.node_id
+  tags      = ["tag:service-jellyfin"]
+}
+
+data "tailscale_device" "silverbullet" {
+  name = "silverbullet.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "silverbullet" {
+  device_id  = data.tailscale_device.silverbullet.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "silverbullet" {
+  device_id = data.tailscale_device.silverbullet.node_id
+  tags      = ["tag:service-silverbullet"]
+}
+
+data "tailscale_device" "transmission" {
+  name = "transmission.soay-puffin.ts.net"
+}
+
+resource "tailscale_device_authorization" "transmission" {
+  device_id  = data.tailscale_device.transmission.node_id
+  authorized = true
+}
+
+resource "tailscale_device_tags" "transmission" {
+  device_id = data.tailscale_device.transmission.node_id
+  tags      = ["tag:service-transmission"]
 }
 
 module "gcp_infrastructure" {
