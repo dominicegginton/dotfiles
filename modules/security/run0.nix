f70fcb7b143e6f810a122ddc2c5095e315156008
Dominic Egginton <dominic.egginton@gmail.com>
2026-08-24 20:39:47 +0100

    chore: update

--- modules/security/run0.nix
+++ modules/security/run0.nix
@@ -1,6 +1,9 @@
 { config, lib, ... }:
 
 {
+  # Enable systemd-run0 for non-WSL hosts
+  security.run0.enable = lib.mkDefault (!config.wsl.enable);
+
   # Configure systemd-run0 for enhanced security
   security.run0.wheelNeedsPassword = lib.mkIf config.security.run0.enable (lib.mkDefault true);
 }
